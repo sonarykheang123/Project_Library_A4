e2b7c61a20fb4b910ffc5f0f57d4e5adbd842043
Sreynich Chhoeurn <sreynich.chhoeurn@student.passerellesnumeriques.org>
2025-06-20 13:47:55 +0700

    create folder pages

---
+++ frontend/src/pages/Memberpage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-semibold p-4 text-center">MEMBERPAGE</h1>
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
