906d09eef13337dd0cd93b07fd720e09f278255c
senghinloem <senghin.loem@student.passerellesnumeriques.org>
2025-06-20 11:14:10 +0700

    added route for Books, Authors, Members

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Books 
+Route::prefix("/books")->group(function() {
+    Route::get("/", [BookController::class, "index"])->name("allBooks");
+    Route::get("/{id}", [BookController::class, "show"]);
+    Route::post("/store", [BookController::class, "store"]);
+    Route::put("/update/{id}", [BookController::class, "update"]);
+    Route::delete("destroy/{id}", [BookController::class, "destroy"]);
+});
+
+//Authors
+Route::prefix("/authors")->group(function() {
+    Route::get("/", [AuthorController::class, "index"])->name("allAuthors");
+    Route::get("/{id}", [AuthorController::class, "show"]);
+    Route::post("/store", [AuthorController::class, "store"]);
+    Route::put("/update/{id}", [AuthorController::class, "update"]);
+    Route::delete("/destroy/{id}", [AuthorController::class, "destroy"]);
+});
+
+//Members
+Route::prefix("/users")->group(function(){
+    Route::get("/", [UserController::class, "index"])->name("allUsers");
+    Route::get("/{id}", [UserController::class, "show"]);
+    Route::post("/store", [UserController::class, "store"]);
+    Route::put("/update/{id}", [UserController::class, "update"]);
+    Route::delete("destroy/{id}", [UserController::class, "destroy"]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
