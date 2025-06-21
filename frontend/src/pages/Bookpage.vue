2d52c11a30ee65e6ec504bb02de5c37e1d8ee103
retthy <retthy.dork@student.passerellesnumeriques.org>
2025-06-21 12:24:16 +0700

    updated style book page and create search books in book-frontend [LBY-#7]

--- frontend/src/pages/Bookpage.vue
+++ frontend/src/pages/Bookpage.vue
@@ -1,5 +1,6 @@
 <template>
-    <h1>Book Page</h1>
+        <h1 class="text-2xl font-bold text-center mb-6 p-5 bg-orange-300 m-3">Book Page</h1>
+        <input type="text" placeholder="Search books..." class="w-80 p-2 border border-gray-300 rounded-md mb-4 m-1 ml-237" />
 </template>
 
 <script setup>
